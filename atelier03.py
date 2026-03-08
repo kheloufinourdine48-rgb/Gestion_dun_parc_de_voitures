6f8d86cc9f63747888d506c473b3451a2876ec5e
kheloufinourdine48-rgb <kheloufinourdine48@gmail.com>
2026-03-08 00:30:41 -0500

    la classe Parc et ses attributs

--- atelier03.py
+++ atelier03.py
@@ -7,3 +7,9 @@ class Voiture:
     def afficher_infos(self):
         print(f"Matricule:{self.matricule} Marque:{self.marque} Couleur: {self.couleur}")
 
+class Parc:
+    def __init__(self, id, adresse, capacite):
+        self.id = id
+        self.adresse = adresse
+        self.capacite = capacite
+        self.listeVoitures = []
